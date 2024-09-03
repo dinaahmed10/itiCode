cd45f9e19c30ac0af6f5f8cae6fc7bdfa04c3c3c
dinaahmed10 <codedinaa@gmail.com>
2024-09-03 18:38:04 +0300

    char and string

---
+++ src/basic/dataTypes.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+		System.out.println(myVar1);
+		System.out.println(myVar2);
+		System.out.println(myVar3+"\n");
+
+		
+		String greeting = "Hello World";
+		System.out.println(greeting);
+	}
+
+}
