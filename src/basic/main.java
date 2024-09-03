9113cd92b0e535f1a08e91cd3b48446c279a96a8
dinaahmed10 <codedinaa@gmail.com>
2024-09-03 18:21:21 +0300

    main

--- src/basic/main.java
+++ src/basic/main.java
@@ -6,7 +6,21 @@ public class main {
 		// TODO Auto-generated method stub
 		 System.out.println("GeeksforGeeks!");
 		 System.out.print("Hello World! ");
-		 System.out.print("I will print on the same line.");
+		 System.out.print("I will print on the same line.\n\n");
+		// This is a comment
+		 System.out.println(3);
+		 System.out.println(358);
+		 System.out.println(50000  +"\n");
+		 
+		 /* The code below will print the words Hello World
+		 to the screen, and it is amazing */
+		 
+		 System.out.println(3 + 3);
+		 System.out.println(2 * 5);
+		 
+		 
+		 
+		 
 
 	}
 
