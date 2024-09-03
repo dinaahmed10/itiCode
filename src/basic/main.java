649e9da02e9201dff75f827cbf8479fc2fb140f6
dinaahmed10 <codedinaa@gmail.com>
2024-09-03 18:15:32 +0300

    main

--- src/basic/main.java
+++ src/basic/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("GeeksforGeeks!");
+		 System.out.print("Hello World! ");
+		 System.out.print("I will print on the same line.");
 
 	}
 
