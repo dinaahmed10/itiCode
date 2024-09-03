eb25a8efecfdbf77de1e0ce98603427977631747
dinaahmed10 <codedinaa@gmail.com>
2024-09-03 18:55:45 +0300

    operator

---
+++ src/basic/Operators.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Operators {
+	public static void main(String[] args) {
+		/*
+		 * 
+		 Arithmetic operators + - * / % ++ --
+         Assignment operators =  +=  -=  *=  /= &=   %=  |= ^=  >>= <<=
+         Comparison operators == != > < >=  <=
+         Logical operators && || !   //!(x < 5 && x < 10)
+         Bitwise operators		  
+		 * 
+		 */
+	}
+}
