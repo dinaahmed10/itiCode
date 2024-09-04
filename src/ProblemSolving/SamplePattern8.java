b925050a5a782ef960374fe7b497ccf111597f4d
dinaahmed10 <codedinaa@gmail.com>
2024-09-04 03:04:32 +0300

    ps

---
+++ src/ProblemSolving/SamplePattern8.java
@@ -0,0 +1,21 @@
+package ProblemSolving;
+
+public class SamplePattern8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+    J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+  
+ * **/
+		System.out.println("    J    a   v     v  a  ");
+		System.out.println("   J   a a   v   v  a a  ");
+		System.out.println("J  J  aaaaa   V V  aaaaa ");
+		System.out.println(" JJ  a     a   V  a     a");
+		
+	}
+
+}
---
+++ src/ProblemSolving/multiplicationTable7.java
@@ -0,0 +1,20 @@
+package ProblemSolving;
+
+import java.util.Scanner;
+
+public class multiplicationTable7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ *Write a Java program that takes a number as input and prints its multiplication table up to 10.
+ * */
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input a number:");
+		int  number=input.nextInt();
+		for(int i=1;i<=10;i++) {
+			System.out.println(i+" x "+number+"= " +(i*number));
+		}
+	}
+
+}
